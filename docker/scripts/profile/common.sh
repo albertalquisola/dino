f055cfe144060e7551c3458bba2ad75c269350a6
AlbertAlquisola <aalquiso@gmail.com>
2017-09-17 13:53:19 -0700

    clean up old node inspector code

--- docker/scripts/profile/common.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export NODE_PATH=/root/tra/node_modules:/root/tra/lib:/root/tra:$NODE_PATH
--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#source prod env variables
-source /root/tra/env/production;
-
 # in production, automatically start the app
 # docker will take care of restart policies in the event that the process or container dies
 cd /root/tra;
-node app.js;
+npm start;
